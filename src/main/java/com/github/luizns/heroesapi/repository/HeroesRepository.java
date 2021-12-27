8acc7f9bb90fe82f9f21bca1bf5e666296f03ad9
Luiz Nascimento <luns.wm@hotmail.com>
2021-12-27 16:54:21 -0300

    Criando gerenciador de herois com Spring Boot|DynamoDB

---
+++ src/main/java/com/github/luizns/heroesapi/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.github.luizns.heroesapi.repository;
+
+import com.github.luizns.heroesapi.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
